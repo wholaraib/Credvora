cea262331a7df5dabe57087f7e6c2bfe2e930568
wholaraib <mohammadlaraib34@gmail.com>
2026-01-06 00:53:32 +0530

    transaction form server action and form schema initialized

--- actions/transaction.js
+++ actions/transaction.js
@@ -1,4 +1,80 @@
-export async function createTransaction(data)
-{
-    // pending
+import { auth } from "@clerk/nextjs/server";
+import { db } from "@/lib/prisma";
+import { revalidatePath } from "next/cache";
+
+const serializeAmount = (obj) => {
+  return {
+    ...obj,
+    amount: obj.amount.toNumber(),
+  };
+};
+export async function createTransaction(data) {
+  try {
+    const { userId } = await auth();
+    if (!userId) throw new Error("Unauthorized");
+
+    const user = await db.user.findUnique({
+      where: {
+        clerkUserId: userId,
+      },
+    });
+
+    if (!user) throw new Error("User not found");
+
+    const account = await db.account.findUnique({
+      where: {
+        id: data.accountId,
+        userId: user.id,
+      },
+    });
+    if (!account) throw new Error("Account not found");
+    const balanceChange = data.type === "INCOME" ? data.amount : -data.amount;
+    const newBalance = account.balance.toNumber() + balanceChange;
+
+    const transaction = await db.$transaction(async (tx) => {
+      const newTransacition = await tx.transaction.create({
+        data: {
+          ...data,
+          userId: user.id,
+          nextRecurringDate:
+            data.isRecurring && data.recurringInterval
+              ? calculateNextRecurringDate(data.date, data.recurringInterval)
+              : null,
+        },
+      });
+
+      await tx.account.update({
+        where: { id: account.id },
+        data: { balance: newBalance },
+      });
+      return newTransacition;
+    });
+
+    revalidatePath("/dashboard");
+    revalidatePath(`/account/${transaction.account.id}`);
+    return {
+      success: true,
+      date: serializeAmount(transaction),
+    };
+  } catch(error) {
+    throw new Error(error.message);
+  }
+}
+
+function calculateNextRecurringDate(startDate, interval) {
+  const date = new Date(startDate);
+  switch (interval) {
+    case "DAILY":
+      date.setDate(date.getDate() + 1);
+      break;
+    case "WEEKLY":
+      date.setDate(date.getDate() + 7);
+      break;
+    case "MONTHLY":
+      date.setMonth(date.getMonth() + 1);
+      break;
+    case "YEARLY":
+      date.setFullYear(date.getFullYear() + 1);
+      break;
+  }
 }
--- app/(main)/transaction/_components/transaction-form.jsx
+++ app/(main)/transaction/_components/transaction-form.jsx
@@ -1,8 +1,29 @@
+"use client";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { transactionSchema } from "@/lib/schema";
+import useFetch from "@/hooks/use-fetch";
+import { createTransaction } from "@/actions/transaction";
 
-const AddTransactionForm = () => {
-  return (
-    <div>AddTransactionForm</div>
-  )
-}
+const AddTransactionForm = ({ accounts, categories }) => {
+  useForm({
+    resolver: zodResolver(transactionSchema),
+    defaultValues: {
+      type: "EXPENSE",
+      amount: "",
+      description: "",
+      accountId: accounts.find((ac) => ac.isDefault)?.id,
+      date: new Date(),
+      isRecurring: false,
+    },
+  });
+  const {
+    loading: transactionLoading,
+    fn: transactionFunc,
+    data: transactionResult,
+  } = useFetch(createTransaction);
 
-export default AddTransactionForm
+  return <div>AddTransactionForm</div>;
+};
+
+export default AddTransactionForm;
