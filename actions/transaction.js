43c3e6f63d2e8db1974e063fa889618d92cb9f2b
wholaraib <mohammadlaraib34@gmail.com>
2026-01-06 22:43:16 +0530

    Bug fix

--- actions/transaction.js
+++ actions/transaction.js
@@ -1,3 +1,4 @@
+"use server"
 import { auth } from "@clerk/nextjs/server";
 import { db } from "@/lib/prisma";
 import { revalidatePath } from "next/cache";
@@ -77,4 +78,27 @@ function calculateNextRecurringDate(startDate, interval) {
       date.setFullYear(date.getFullYear() + 1);
       break;
   }
+}
+
+
+export async function getTransaction(id) {
+  const { userId } = await auth();
+  if (!userId) throw new Error("Unauthorized");
+
+  const user = await db.user.findUnique({
+    where: { clerkUserId: userId },
+  });
+
+  if (!user) throw new Error("User not found");
+
+  const transaction = await db.transaction.findUnique({
+    where: {
+      id,
+      userId: user.id,
+    },
+  });
+
+  if (!transaction) throw new Error("Transaction not found");
+
+  return serializeAmount(transaction);
 }
