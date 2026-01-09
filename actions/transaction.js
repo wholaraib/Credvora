9bf9696fe7e3d938c7a3c345d4daf33e3fece19a
Mohammad Laraib <100714696+wholaraib@users.noreply.github.com>
2026-01-09 22:20:41 +0530

    added rate limiting

--- actions/transaction.js
+++ actions/transaction.js
@@ -2,6 +2,8 @@
 import { auth } from "@clerk/nextjs/server";
 import { db } from "@/lib/prisma";
 import { revalidatePath } from "next/cache";
+import { request } from "@arcjet/next";
+import aj from "@/lib/arcjet";
 
 const serializeAmount = (obj) => ({
   ...obj,
@@ -12,7 +14,30 @@ export async function createTransaction(data) {
   try {
     const { userId } = await auth();
     if (!userId) throw new Error("Unauthorized");
+    
+    const req = await request();
 
+    const decision = await aj.protect(req, {
+      userId,
+      requested: 1, // Specify how many tokens to consume
+    });
+
+    if (decision.isDenied()) {
+      if (decision.reason.isRateLimit()) {
+        const { remaining, reset } = decision.reason;
+        console.error({
+          code: "RATE_LIMIT_EXCEEDED",
+          details: {
+            remaining,
+            resetInSeconds: reset,
+          },
+        });
+
+        throw new Error("Too many requests. Please try again later.");
+      }
+
+      throw new Error("Request blocked");
+    }
 
     const user = await db.user.findUnique({
       where: { clerkUserId: userId },
@@ -105,4 +130,5 @@ function calculateNextRecurringDate(startDate, interval) {
       date.setFullYear(date.getFullYear() + 1);
       break;
   }
+  return date;
 }
